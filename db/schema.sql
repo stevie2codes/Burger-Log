e15c2d4532e752bbc3ec60cdfb121fc42a8e3d6a
stevie2codes <stevie2codes@gmail.com>
2020-02-01 15:02:28 -0600

    added sequelize and mysql2 and updated schema to use sequelize

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,16 @@
-CREATE DATABASE burgers_db;
+DROP DATABASE IF EXISTS `burgers_db`;
+CREATE DATABASE `burgers_db`;
 
-USE burgers_db;
 
-CREATE TABLE burgers(
-    id NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    burger_name VARCHAR(40),
-    devoured BOOLEAN DEFAULT FALSE
-);
+
+
+
+-- CREATE DATABASE burgers_db;
+
+-- USE burgers_db;
+
+-- CREATE TABLE burgers(
+--     id NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     burger_name VARCHAR(40),
+--     devoured BOOLEAN DEFAULT FALSE
+-- );
