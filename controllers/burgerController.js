60171f44bf2b99595075e04b8b4eff3518aa05c2
stevie2codes <stevie2codes@gmail.com>
2020-02-02 16:13:45 -0600

    Got server connection working and added some sample html to handlebars

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -1,14 +1,20 @@
-const Burger = require('../models/burger');
+const Burger = require('../models/burger.js');
 
 module.exports = function(app){
+
+    app.get("/", function(req, res) {
+        Burger.findAll({}).then(function(results) {
+          res.render("index",{burger:results});
+        });
+      });
     //Get all burger names
     app.get("/api/burgerName",(req,res)=>{
         Burger.findAll({}).then((results)=> res.json(results));
     });
         // Add a new burger
-    app.post("/api/newBurger",(req,res)=> {
-        Burger.create({
-            burger_name: req.body.burger_name
-        }).then((results)=> res.json(results));
-    });
+    // app.post("/api/newBurger",(req,res)=> {
+    //     Burger.create({
+    //         burger_name: req.body.burger_name
+    //     }).then((results)=> res.json(results));
+    // });
 }
--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-let routes = require("./controllers/burgerController.js");
+require("./controllers/burgerController")(app);
 
-app.use(routes);
+
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
